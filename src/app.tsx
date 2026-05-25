d51774af01cb158fb115bcb9dfaf0038123976a2
Pablo Corso <pabcorso@gmail.com>
2026-05-25 15:45:07 +0200

    Keep only storybook for testing

--- src/app.tsx
+++
@@ -1,6 +0,0 @@
-import { footballShowcaseBoard } from "./examples/football/football-showcase-board";
-import { FootballBoardEditor } from "./react";
-
-export default function App() {
-  return <FootballBoardEditor initialBoard={footballShowcaseBoard} />;
-}
